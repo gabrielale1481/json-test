ef43aa978478aefb014e1a9b421a18ee7b3da4d3
gabrielale1481 <gabrielale1481@gmail.com>
2021-03-18 08:10:12 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const { promisify } = require("util");
+const { exec } = require("child_process");
+
+const execute = function( command ){
+
+    const _exec = cmd => promisify(exec)(cmd);
+
+    if( Array.isArray(command) ) return new Promise(async function(resolve, reject){
+        const result = [];
+        for( let cmd of command ) result.push(await _exec(cmd));
+        resolve(result);
+    })
+
+    return _exec(command);
+
+}
+
+const formatOutput = output => output.replace(/\n\r/g, '');
+
+const gitCommands = [
+    'git add .',
+    'git commit -m "initial commit"',
+    'git push origin main'
+]
+
+execute(gitCommands).then(function([commit, push]){
+
+    const error = commit.stderr ?? push.stderr;
+
+    console.log(error);
+
+    if( error ) throw error;
+
+    console.log(formatOutput(commit.stdout), "\n", formatOutput(push.stdout));
+
+}).catch(e => console.log(e.message))
